3a6422a0e2df19ad311e8a3b4eb7761a4e323034
wick7 <wickersham.craig@gmail.com>
2019-02-09 14:12:14 -0800

    update

--- server.js
+++ server.js
@@ -46,184 +46,184 @@ app.get("/", function (req, res) {
   res.render('index')
 });
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function (req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.npr.org/sections/politics/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    var scraped = [];
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article").each(function (i, element) {
-      // Save an empty result object
+// // A GET route for scraping the echoJS website
+// app.get("/scrape", function (req, res) {
+//   // First, we grab the body of the html with axios
+//   axios.get("https://www.npr.org/sections/politics/").then(function (response) {
+//     // Then, we load that into cheerio and save it to $ for a shorthand selector
+//     var $ = cheerio.load(response.data);
+//     var scraped = [];
+//     // Now, we grab every h2 within an article tag, and do the following:
+//     $("article").each(function (i, element) {
+//       // Save an empty result object
 
-      var result = {};
+//       var result = {};
 
-      result.title = $(this)
-        .find(".item-info")
-        .find(".title")
-        .children("a")
-        .text();
-      result.desc = $(this)
-        .find(".teaser")
-        .children("a")
-        .text();
-      result.imgLink = $(this)
-        .find(".imagewrap")
-        .children("a")
-        .children("img")
-        .attr('src');
-      result.link = $(this)
-        .children(".item-info")
-        .children(".title")
-        .children("a")
-        .attr('href');
-      result.date = $(this)
-        .find(".date")
-        .text();
-        if(result.imgLink === undefined) {
+//       result.title = $(this)
+//         .find(".item-info")
+//         .find(".title")
+//         .children("a")
+//         .text();
+//       result.desc = $(this)
+//         .find(".teaser")
+//         .children("a")
+//         .text();
+//       result.imgLink = $(this)
+//         .find(".imagewrap")
+//         .children("a")
+//         .children("img")
+//         .attr('src');
+//       result.link = $(this)
+//         .children(".item-info")
+//         .children(".title")
+//         .children("a")
+//         .attr('href');
+//       result.date = $(this)
+//         .find(".date")
+//         .text();
+//         if(result.imgLink === undefined) {
 
-        }else {
-          scraped.push(result)
-        }
+//         }else {
+//           scraped.push(result)
+//         }
         
 
-      // db.Article.create(result)
-      //   .then(function(dbArticle) {
-      //     // View the added result in the console
-      //     console.log(dbArticle);
-      //   })
-      //   .catch(function(err) {
-      //     // If an error occurred, log it
-      //     console.log(err);
-      //   });
-    });
+//       // db.Article.create(result)
+//       //   .then(function(dbArticle) {
+//       //     // View the added result in the console
+//       //     console.log(dbArticle);
+//       //   })
+//       //   .catch(function(err) {
+//       //     // If an error occurred, log it
+//       //     console.log(err);
+//       //   });
+//     });
 
 
-    // Send a message to the client
-    res.render('index', {data: scraped});
+//     // Send a message to the client
+//     res.render('index', {data: scraped});
     
-  });
-});
+//   });
+// });
 
 
-app.get("/articles", function (req, res) {
+// app.get("/articles", function (req, res) {
   
-  db.Article.find({})
-        .then(function (dbArticle) {
-          // var tag = []
-          //  for(var i of dbArticle) {
-          //    tag['tag'] = 'modal-tag' + i._id
-          //  }
-          //  console.log(dbArticle)
+//   db.Article.find({})
+//         .then(function (dbArticle) {
+//           // var tag = []
+//           //  for(var i of dbArticle) {
+//           //    tag['tag'] = 'modal-tag' + i._id
+//           //  }
+//           //  console.log(dbArticle)
          
-          res.render('saved', {data: dbArticle})
-        })
-        .catch(function (err) {
-          // If an error occurred, send it to the client
-          console.log(err);
-        });
+//           res.render('saved', {data: dbArticle})
+//         })
+//         .catch(function (err) {
+//           // If an error occurred, send it to the client
+//           console.log(err);
+//         });
   
-});
+// });
 
-// Route for getting all Articles from the db
-app.post("/save", function (req, res) {
+// // Route for getting all Articles from the db
+// app.post("/save", function (req, res) {
   
-  console.log(req.body)
+//   console.log(req.body)
 
-  db.Article.create(req.body)
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      console.log(dbArticle)
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      console.log(err);
-    });
-});
+//   db.Article.create(req.body)
+//     .then(function (dbArticle) {
+//       // If we were able to successfully find Articles, send them back to the client
+//       console.log(dbArticle)
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       console.log(err);
+//     });
+// });
 
-app.put("/delete", function (req, res) {
+// app.put("/delete", function (req, res) {
   
-  console.log(req.body.id)
+//   console.log(req.body.id)
 
-  db.Article.remove({_id:req.body.id})
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      console.log(dbArticle)
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      console.log(err);
-    });
-});
-
-
-app.delete("/delete-all", function (req, res) {
+//   db.Article.remove({_id:req.body.id})
+//     .then(function (dbArticle) {
+//       // If we were able to successfully find Articles, send them back to the client
+//       console.log(dbArticle)
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       console.log(err);
+//     });
+// });
 
 
-  db.Article.remove({})
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      console.log("All Deleted")
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      console.log(err);
-    });
-});
+// app.delete("/delete-all", function (req, res) {
 
-app.put("/delete-note", function (req, res) {
+
+//   db.Article.remove({})
+//     .then(function (dbArticle) {
+//       // If we were able to successfully find Articles, send them back to the client
+//       console.log("All Deleted")
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       console.log(err);
+//     });
+// });
+
+// app.put("/delete-note", function (req, res) {
   
-  console.log(req.body.id)
+//   console.log(req.body.id)
 
-  db.Note.remove({_id:req.body.id})
-    .then(function (dbNote) {
-      // If we were able to successfully find Articles, send them back to the client
-      console.log(dbNote)
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      console.log(err);
-    });
-});
+//   db.Note.remove({_id:req.body.id})
+//     .then(function (dbNote) {
+//       // If we were able to successfully find Articles, send them back to the client
+//       console.log(dbNote)
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       console.log(err);
+//     });
+// });
 
-app.post("/new-note", function (req, res) {
+// app.post("/new-note", function (req, res) {
   
-  console.log({'title': req.body.title, 'body': req.body.body}, req.body.artId)
+//   console.log({'title': req.body.title, 'body': req.body.body}, req.body.artId)
 
-  db.Note.create({'title': req.body.title, 'body': req.body.body})
-    .then(function (dbNote) {
-      // If we were able to successfully find Articles, send them back to the client
-      console.log(req.body.artId)
-      return db.Article.findOneAndUpdate({ _id: req.body.artId }, { $push: { note: dbNote._id }}, { new: true });
-    }).then(function(dbArticle){
-      console.log(dbArticle)
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      console.log(err);
-    });
-});
+//   db.Note.create({'title': req.body.title, 'body': req.body.body})
+//     .then(function (dbNote) {
+//       // If we were able to successfully find Articles, send them back to the client
+//       console.log(req.body.artId)
+//       return db.Article.findOneAndUpdate({ _id: req.body.artId }, { $push: { note: dbNote._id }}, { new: true });
+//     }).then(function(dbArticle){
+//       console.log(dbArticle)
+//     })
+//     .catch(function (err) {
+//       // If an error occurred, send it to the client
+//       console.log(err);
+//     });
+// });
 
-app.get("/article-notes/:id", function(req, res) {
-  console.log(req.params.id)
+// app.get("/article-notes/:id", function(req, res) {
+//   console.log(req.params.id)
 
   
 
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
+//   db.Article.findOne({ _id: req.params.id })
+//     .populate("note")
+//     .then(function(dbArticle) {
+//       // If we were able to successfully find an Article with the given id, send it back to the client
 
-      console.log(dbArticle)
+//       console.log(dbArticle)
       
-      res.render('listing', {data: dbArticle})
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+//       res.render('listing', {data: dbArticle})
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
 
 app.listen(PORT, function () {
